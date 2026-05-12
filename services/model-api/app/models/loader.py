c341ac34f5c301a0e056986d8de43f25278233de
simGPT <simGPT02@gmail.com>
2026-05-13 02:38:38 +0900

    🚀 Deploy: MLflow 기반 S3 모델 저장 및 로드 환경 구축 (#18)
    * 🔀 Merge develop into infra/prometheus-grafana (#12)
    
    * :wrench:Settings: model-api 매트릭 수집 설정
    
    * :sparkles:Feat: /metrics 앤드포인트 열기 및 수집할 데이터 정의
    
    * :heavy_plus_sign:Dependency: prometheus 의존성 추가
    
    * :bug:Fix: cd 시에 이미지 태그 업데이트 하도록 수정(v1 -> 커밋SHA) (#14)
    
    * ✨ Feat: MLflow 기반 S3 모델 저장 및 로드 환경 구축 (#17)
    
    * :wrench:Settings: MLflow deployment 및 service 설정
    
    * :wrench:Settings: mlflow 모델 성능 저장용 deployement 및 service 설정
    
    * :heavy_plus_sign:Dependency: mlflow 의존성 추가
    
    * :recycle:Refactor: 모델 mlflow를 이용한 로드로 수정
    
    * :recycle:Refactor: 모델 학습 후 mlflow에 모델 성능 기록 및 모델 등록
    
    * :wrench:Settings: mlflow 서버 주소 명시적으로 지정
    
    * :wrench:Settings: mlflow 이미지 버전 변경
    
    * :wrench:Settings: env 추가
    
    * :heavy_plus_sign:Dependency: 학습 의존성에 boto3 추가하기

--- services/model-api/app/models/loader.py
+++ services/model-api/app/models/loader.py
@@ -1,26 +1,20 @@
 import os
-import torch
-from app.models.mnist_model import get_model
+import mlflow.pytorch
 
 
 _model_cache: dict = {}
 
 
-def load_model(model_name: str, version: str, artifact_dir: str = "artifacts"):
-    cache_key = f"{model_name}_{version}" # 캐시 있으면 그거 먼저 확인
+def load_model(model_name: str, version: str):
+    cache_key = f"{model_name}_{version}"
     if cache_key in _model_cache:
         return _model_cache[cache_key]
 
-    path = os.path.join(artifact_dir, f"model_{version}.pth")
-    if not os.path.exists(path):
-        raise FileNotFoundError(f"모델 파일 없음: {path}")
+    mlflow_uri = os.getenv("MLFLOW_TRACKING_URI", "http://mlflow:5000")
+    mlflow.set_tracking_uri(mlflow_uri)
 
-    if model_name == "mnist":
-        model = get_model()
-    else:
-        raise ValueError(f"지원하지 않는 모델: {model_name}")
-
-    model.load_state_dict(torch.load(path, map_location="cpu"))
+    model_uri = f"models:/{model_name}-{version}/latest"
+    model = mlflow.pytorch.load_model(model_uri)
     model.eval()
 
     _model_cache[cache_key] = model
