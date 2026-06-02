00c3e9889a43b4a0afbae890461756da411f0419
simGPT <simGPT02@gmail.com>
2026-06-02 16:14:13 +0900

    🚀 Deploy: loader.py 중복 함수 제거로 churn 예측 오류 수정 (#46)
    * :recycle:Refactor: churn 모델 학습 데이터 .gitignore 제거
    
    * :sparkles:Feat: churn 모델 학습 관련 데이터 csv파일 추가
    
    * :bug:Fix: aws 자격증명 추가 (#43)
    
    * :bug:Fix: churn loader 구버전 함수 제거 (#45)

--- services/model-api/app/models/loader.py
+++ services/model-api/app/models/loader.py
@@ -35,18 +35,3 @@ def load_churn_model(model_name: str):
 
     _model_cache[cache_key] = model
     return model
-
-# mlflow에서 고객 이탈 예측 모델 로드하는 함수
-def load_churn_model(model_name: str, version: str):
-    cache_key = f"{model_name}_{version}_sklearn"
-    if cache_key in _model_cache:
-        return _model_cache[cache_key]
-
-    mlflow_uri = os.getenv("MLFLOW_TRACKING_URI", "http://mlflow:5000")
-    mlflow.set_tracking_uri(mlflow_uri)
-
-    model_uri = f"models:/{model_name}-{version}/latest"
-    model = mlflow.sklearn.load_model(model_uri)
-
-    _model_cache[cache_key] = model
-    return model
